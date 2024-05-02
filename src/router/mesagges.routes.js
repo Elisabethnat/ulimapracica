bd1075d33b3727a8053b315981fa691791779003
Elisabeth <lelie_angel@hotmail.com>
2024-05-02 09:17:53 -0300

    cambiosrou

---
+++ src/router/mesagges.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import messagesController from "../controllers/message.controller.js";
+
+const messageRouter = Router();
+
+messageRouter.get('/', messagesController.getMessages);
+messageRouter.post('/', messagesController.postMessages);
+
+export default messageRouter;
